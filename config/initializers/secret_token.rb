a3c691aec713de8f6c602c65e3cb43d128368665
Digital Traffic <digitaltraffic@digitraf2.local>
2014-03-11 10:49:10 -0500

    baseline photo model

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Photobomb::Application.config.secret_token = 'c97baf785f97cda9d6c13e905b7b85595ed12ffcf9f95d3696f0d8a44c6f5c58e111f592137740556b73305551d36f3b352bbcfa2b9ed133b482c97314dd8dcd'
